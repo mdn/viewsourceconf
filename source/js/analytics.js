864dd23173f9d830738dd50c6bf1e64db5946371
Stephanie Hobson <sjhobson@gmail.com>
2016-05-10 10:23:11 -0700

    Update favicons
    All compressed using ImageOptim, updated icons smaller than 76x76 with thicker line version.

--- source/js/analytics.js
+++ source/js/analytics.js
@@ -62,14 +62,145 @@
         },
 
 
-        trackOutboundLinks: function() {
-            document.body.addEventListener('click', 'a', function (event) {
-                var $this = $(this);
+        trackClicks: function() {
+            console.log('trackLinks executed');
+            // capture all click events
+            document.addEventListener('click', function (event) {
+            console.log('click detected');
+// TODO: don't do anything if default was prevented, hijax will handle that
+
+                // was it a link?
+                event = event || window.event;
+                var local = window.location.protocol + '//' + window.location.host;
+                var trigger = event.target.tagName;
+                var parent = event.target.parentNode.tagName;
+                console.log('event', event);
+                if(trigger == 'A' || parent == 'A') {
+                    var link = (trigger == 'A') ? event.target : event.target.parentNode;
+                    var href;
+                    if (link.href) {
+                        href = link.href;
+                    } else {
+                        // no href, abort, abort
+                        return;
+                    }
+                    console.log('link detected', link);
+                    console.log('link href', link.href);
+                    console.log('link href attr', link.getAttribute('href'));
+                    console.log('local', local);
+                    var category = 'link';
+                    var action;
+                    var label;
+                    var callback;
+
+                    // TODO: callback with timeout if leaving page
+
+                    // link away from site
+                    if(href.indexOf(local) == -1) {
+                        console.log('link away from site');
+                    }
+
+                    // email link
+                    if(href.indexOf('mailto:') == -1) {
+                        console.log('email link');
+                    }
+
+                    // link stays on current page
+                    if(path == path) {
+                        console.log('link leads to current page');
+                    }
+
+                    // check if in header or footer
+                    var headerNode = document.getElementById('header');
+                    var footerNode = document.getElementById('footer');
+                    // header/footer
+                    var inHeader = false;
+                    var inFooter = false;
+                    if(headerNode) {
+                        inHeader = headerNode.contains(link);
+                    }
+                    if(footerNode) {
+                        inFooter = footerNode.contains(link);
+                    }
+
+                    if(inHeader || inFooter) {
+                        var which = inHeader ? 'header' : 'footer';
+                        //trackEvent('nav', which, label);
+                        console.log('trackEvent','nav', which, link.href);
+                    }
+
+
+                    var filetypes = /\.(zip|exe|dmg|pdf|doc.*|xls.*|ppt.*|mp3|txt|rar|wma|mov|avi|wmv|flv|wav)$/i;
+                    var baseHref = '';
+
+                    var track = true;
+                    var isThisDomain = href.indexOf(local);
+                    if (!href.match(/^javascript:/i)) {
+                        var elEv = []; elEv.value=0, elEv.non_i=false;
+                        if (href.match(/^mailto\:/i)) {
+                            elEv.category = "email";
+                            elEv.action = "click";
+                            elEv.label = href.replace(/^mailto\:/i, '');
+                            elEv.loc = href;
+                        }
+                        else if (href.match(filetypes)) {
+                        var extension = (/[.]/.exec(href)) ? /[^.]+$/.exec(href) : undefined;
+                            elEv.category = "download";
+                            elEv.action = "click-" + extension[0];
+                            elEv.label = href.replace(/ /g,"-");
+                            elEv.loc = baseHref + href;
+                        }
+                        else if (href.match(/^https?\:/i) && !isThisDomain) {
+                            elEv.category = "external";
+                            elEv.action = "click";
+                            elEv.label = href.replace(/^https?\:\/\//i, '');
+                            elEv.non_i = true;
+                            elEv.loc = href;
+                        }
+                        else if (href.match(/^tel\:/i)) {
+                            elEv.category = "telephone";
+                            elEv.action = "click";
+                            elEv.label = href.replace(/^tel\:/i, '');
+                            elEv.loc = href;
+                        }
+                        else {
+                            track = false;
+                        }
+
+                        //track
+                    }
+                    // what kind of link?
+
+                        // anchor
+                            // anchor
+                            // path + #hash
+                        // file
+                            // file
+                            // href
+                        // email
+                            // email
+                            // email address
+                        // external
+                            // external
+                            // full URL
+                        // else regular page nav?
+
+                    //trackEvent(category, action, label);
+                    console.log('trackEvent', category, action, label);
+                }
+
+
+
+                // anchor
+                // file
+                // email
+                // header or footer nav
+/*
 
                 var host = this.hostname;
                 if(host && host !== location.hostname) {
-                    var newTab = (this.target === '_blank' || event.metaKey || event.ctrlKey);
-                    var href = this.href;
+                    var newTab = (trigger.target === '_blank' || event.metaKey || event.ctrlKey);
+                    var href = trigger.href;
                     var callback = function() {
                         window.location = href;
                     };
@@ -86,7 +217,8 @@
                         analytics.trackEvent(data, callback);
                     }
                 }
-            });
+                            */
+            }, false);
         },
 
         /*
@@ -96,9 +228,13 @@
         trackClientErrors: function() {
             window.addEventListener('error', function (err) {
                 var lineAndColumnInfo = err.colno ? ' line:' + err.lineno +', column:'+ err.colno : ' line:' + err.lineno;
-                analytics.trackEvent('JavaScript Error', err.message , err.filename + ':' + lineAndColumnInfo);
+                analytics.trackEvent('Error', err.message , err.filename + ':' + lineAndColumnInfo);
             });
         },
 
     };
+
+    window.vs.analytics.trackClicks();
 })();
+
+
--- source/js/speakers.js
+++ source/js/speakers.js
@@ -1,100 +1,101 @@
 (function() {
     'use strict';
-    var speakers_list = document.getElementById('speakers_list');
+    var speakersList = document.getElementById('speakers_list');
     var speakers;
-    if(speakers_list !== null) {
-        speakers = speakers_list.querySelectorAll('.speaker');
+    if(speakersList !== null) {
+        speakers = speakersList.querySelectorAll('.speaker');
     } else {
         return;
     }
 
     Array.prototype.forEach.call(speakers, function(element, i) {
-        var speaker_id = element.id;
+        var speakerId = element.id;
         // create button to close speaker_info
         var button = document.createElement('button');
         button.setAttribute('type', 'button');
-        button.setAttribute('aria-controls', speaker_id + '_info');
+        button.setAttribute('aria-controls', speakerId + '_info');
         button.setAttribute('aria-expanded', 'false');
         button.classList.add('speaker_close');
         button.appendChild(document.createTextNode('Return to speaker list.'));
         //add listenter
         button.addEventListener('click', function() {
-            speaker_close(speaker_id);
-        });
+            speakerClose(speakerId);
+        }, false);
         // append button to speaker_test
-        var speaker_text = element.querySelectorAll('.speaker_text');
-        Array.prototype.forEach.call(speaker_text, function(element, i){
+        var speakerText = element.querySelectorAll('.speaker_text');
+        Array.prototype.forEach.call(speakerText, function(element, i){
             element.appendChild(button);
         });
 
         // add listners & ARIA on speaker photos
-        var speaker_links = element.querySelectorAll('.speaker_pic a');
-        Array.prototype.forEach.call(speaker_links, function(element, i) {
-            element.setAttribute('aria-controls', speaker_id + '_info');
-            element.setAttribute('href', '#' + speaker_id);
+        var speakerLinks = element.querySelectorAll('.speaker_pic a');
+        Array.prototype.forEach.call(speakerLinks, function(element, i) {
+            element.setAttribute('aria-controls', speakerId + '_info');
+            element.setAttribute('href', '#' + speakerId);
             element.setAttribute('aria-expanded', 'false');
             element.addEventListener('click', function(event) {
-                speaker_toggle(event, speaker_id);
-            });
+                speakerToggle(event, speakerId);
+            }, false);
         });
     });
 
-    function speaker_toggle(event, speaker_id) {
-        var speaker = document.getElementById(speaker_id);
-        var is_active = speaker.classList.contains('js-active');
-        if(is_active) {
+    function speakerToggle(event, speakerId) {
+        var speaker = document.getElementById(speakerId);
+        var isActive = speaker.classList.contains('js-active');
+        if(isActive) {
             // if open, close it
-            speaker_close(speaker_id);
+            speakerClose(speakerId);
         } else {
             // otherwise close all others
-            var active_speakers = speakers_list.querySelectorAll('.js-active');
-            Array.prototype.forEach.call(active_speakers, function(element, i) {
-                var element_id = element.id;
-                speaker_close(element_id);
+            var activeSpeakers = speakersList.querySelectorAll('.js-active');
+            Array.prototype.forEach.call(activeSpeakers, function(element, i) {
+                var elementId = element.id;
+                speakerClose(elementId);
             });
             // open it
-            speaker_open(speaker_id);
+            speakerOpen(speakerId);
         }
     }
 
-    function speaker_open(speaker_id) {
+    function speakerOpen(speakerId) {
         // add active class to list
-        speakers_list.classList.add('js-active');
-        var speaker = document.getElementById(speaker_id);
+        speakersList.classList.add('js-active');
+        var speaker = document.getElementById(speakerId);
         // get minHeight (.speaker_pic + .speaker_info)
         var pic = speaker.querySelectorAll('.speaker_pic');
-        var pic_height = pic[0].offsetHeight;
+        var picHeight = pic[0].offsetHeight;
         var info = speaker.querySelectorAll('.speaker_info');
-        var info_height = info[0].offsetHeight;
-        var speaker_height = pic_height + info_height;
+        var infoHeight = info[0].offsetHeight;
+        var speakerHeight = picHeight + infoHeight;
         // add class
         speaker.classList.add('js-active');
         // set minHeight
-        speaker.style.minHeight = speaker_height + 'px';
-        info[0].style.minHeight = info_height + 'px';
+        speaker.style.minHeight = speakerHeight + 'px';
+        info[0].style.minHeight = infoHeight + 'px';
         // set aria-expanded to true
-        var controls = speaker.querySelectorAll('[aria-controls='+ speaker_id +'_info]');
+        var controls = speaker.querySelectorAll('[aria-controls='+ speakerId +'_info]');
         Array.prototype.forEach.call(controls, function(element, i) {
             element.setAttribute('aria-expanded', 'true');
         });
 
         if(window.vs.analytics) {
             window.vs.analytics.trackEvent({
-                category: 'speaker-open',
-                action: speaker_id
+                category: 'expand',
+                action: 'speaker',
+                label: speakerId
             });
         }
     }
 
-    function speaker_close(speaker_id) {
+    function speakerClose(speakerId) {
         // remove active class from parent
-        speakers_list.classList.remove('js-active');
+        speakersList.classList.remove('js-active');
         // remove active class from speaker
-        var speaker = document.getElementById(speaker_id);
+        var speaker = document.getElementById(speakerId);
         speaker.classList.remove('js-active');
         speaker.style.minHeight = '0px';
         // set aria-expanded to false
-        var controls = speaker.querySelectorAll('[aria-controls='+ speaker_id +'_info]');
+        var controls = speaker.querySelectorAll('[aria-controls='+ speakerId +'_info]');
         Array.prototype.forEach.call(controls, function(element, i) {
             element.setAttribute('aria-expanded', 'false');
         });
