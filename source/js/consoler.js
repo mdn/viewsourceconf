42750a259fff70a5f37fb518a9bd0348d9840bef
hoosteeno <justin@tentacu.com>
2016-05-10 21:11:39 -0600

    Styled console message pointing to the repo

---
+++ source/js/consoler.js
@@ -0,0 +1,4 @@
+;(function() {
+    'use strict';
+    console.log("%cWelcome to the guts of our website. Want to see or contribute to the code? https://github.com/mdn/viewsourceconf", "color: #fff;background-color: #313131;padding: 5px;display: block;");
+})();
