c6d3e4f29d848b72dc4af655ef7dc29d86e53d0d
hoosteeno <justin@tentacu.com>
2016-05-10 21:23:50 -0600

    Refine consoler

--- source/js/consoler.js
+++ source/js/consoler.js
@@ -1,4 +1,4 @@
 ;(function() {
     'use strict';
-    console.log("%cWelcome to the guts of our website. Want to see or contribute to the code? https://github.com/mdn/viewsourceconf", "color: #fff;background-color: #313131;padding: 5px;display: block;");
+    console.log("%cWelcome to the guts of our website. We're glad you're here! Want to see or contribute to the code? https://github.com/mdn/viewsourceconf", "color: #fff;background-color: #4D4E53;padding: 5px;display: block;line-height: 1.75em;");
 })();
